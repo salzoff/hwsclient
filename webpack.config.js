f743ffd1b69c5b1dd8b4f46eef7b0bb57f194cfa
salzmann <marcus.salzmann@peakwork.com>
2019-05-03 18:56:52 +0200

    latest

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,9 @@ const path = require('path');
 module.exports = {
     entry: {
         service: './src/index.js',
-        client: './src/client/index.js'
+        client: './src/client/index.js',
+        requestClasses: './src/classes/request/index.js',
+        responseClasses: './src/classes/response/index.js'
     },
     target: 'node',
     output: {
