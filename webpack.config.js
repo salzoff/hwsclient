9957d62e943e78ef6f167a5b36f6e5116b9817c2
salzmann <marcus.salzmann@peakwork.com>
2019-05-21 03:56:55 +0200

    init

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,14 @@ const path = require('path');
 module.exports = {
     entry: {
         service: './src/run.js',
-        index: './src/index.js'
+        index: './src/index.js',
+        client: './src/client/index.js'
     },
     target: 'node',
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
         library: '[name]',
-        libraryExport: 'default',
         libraryTarget: 'umd',
         globalObject: 'typeof self !== \'undefined\' ? self : this'
     },
